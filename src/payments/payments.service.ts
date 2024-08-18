af5cadf72f76ac8331b5227d8820cb9d49d5eaff
Jesús <jesusguerreropaniagua6@gmail.com>
2024-08-18 18:12:39 +0200

    feat: create payment session

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -8,7 +8,26 @@ export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
   async createPaymentSession() {
-    const session = await this.stripe.checkout.sessions.create({});
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {},
+      },
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "T-shirt",
+            },
+            unit_amount: 2000, // price multiplied by 100
+          },
+          quantity: 2,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3003/payments/success",
+      cancel_url: "http://localhost:3003/payments/cancel",
+    });
 
     return session;
   }
